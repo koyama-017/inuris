38e847cf38372ce169b19382c380a1366bd991f7
koyama-017 <orusan1104@icloud.com>
2023-10-13 21:54:50 +0900

    change to gz

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
     // dataUrl: "/Build/public.data",
     // frameworkUrl: "/Build/public.framework.js",
     // codeUrl: "/Build/public.wasm",                                                                                                                                          
+    dataUrl: "/Build/public.data.gz",
+    frameworkUrl: "/Build/public.framework.js.gz",
     loaderUrl: "/Build/public.loader.js",
-    dataUrl: "/Build/public.data.br",
-    frameworkUrl: "/Build/public.framework.js.br",
-    codeUrl: "/Build/public.wasm.br",                                                                                                                                          
+    codeUrl: "/Build/public.wasm.gz",                                                                                                                                          
   })
   return (
       <Unity unityProvider={unityProvider} className="w-[960px] h-[540px] top-1/2 absolute -translate-y-1/2 left-1/2 -translate-x-1/2"/>
