0a0dd113a6f3abb1eb5efe6231dd316547411b6a
koyama-017 <orusan1104@icloud.com>
2023-10-13 21:57:18 +0900

    delete zip

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
     // dataUrl: "/Build/public.data",
     // frameworkUrl: "/Build/public.framework.js",
     // codeUrl: "/Build/public.wasm",                                                                                                                                          
-    dataUrl: "/Build/public.data.gz",
-    frameworkUrl: "/Build/public.framework.js.gz",
+    dataUrl: "/Build/public.data",
+    frameworkUrl: "/Build/public.framework.js",
     loaderUrl: "/Build/public.loader.js",
-    codeUrl: "/Build/public.wasm.gz",                                                                                                                                          
+    codeUrl: "/Build/public.wasm",                                                                                                                                          
   })
   return (
       <Unity unityProvider={unityProvider} className="w-[960px] h-[540px] top-1/2 absolute -translate-y-1/2 left-1/2 -translate-x-1/2"/>
